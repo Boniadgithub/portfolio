282146f72da36f4093cb0042a61e90d49f8ae22c
Bonsa Adugna <136079152+Boniadgithub@users.noreply.github.com>
2025-05-12 12:39:50 -0700

    My seventh commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  /* var typed= new typed(".text",{
+    Strings:["Frontend Developer","UI/UX Designer","web Developer"],
+    typedSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});*/
